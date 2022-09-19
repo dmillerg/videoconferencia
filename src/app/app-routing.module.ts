883651ad0a6d98c1adc0f30652ca1b422821fcab
Kuroko <dmillergg@gmail.com>
2022-09-19 16:35:33 -0700

    ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { VideosComponent } from './pages/videos/videos.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/videos', pathMatch: 'full' },
   { path: 'inicio', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'videos', component: VideosComponent },
   
 ];
 
