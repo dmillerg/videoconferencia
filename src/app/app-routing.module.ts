6a6c6b2b554661a1fd8889e5fcc8cb8d5efc9838
Daniel <dmillergg@gmail.com>
2022-09-19 14:36:47 -0400

    ajustess

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'inicio', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
