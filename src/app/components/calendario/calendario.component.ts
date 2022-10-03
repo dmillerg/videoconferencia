9bd9aa4a6077ef54609e87c6162986041491dd58
Kuroko <dmillergg@gmail.com>
2022-10-03 13:46:24 -0700

    ajustes

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import * as moment from 'moment';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -16,19 +16,20 @@ export class CalendarioComponent implements OnInit {
   anno: number = this.now.year();
   month: number = this.now.month();
   day: number = this.now.date();
-  selected: any = this.now;
-  not_allowed: any[] = [moment('2022-09-23')];
-  dates: any[] = [
-    { id: 5, nombre: 'Primera', descripcion: 'Esta es la primera videoconferencia planificada', hora_inicio: '09:00 AM', hora_fin: '11:30 AM', fecha: '2022-09-29', mannana: 1, tarde: 0 },
-    { id: 5, nombre: 'Primera', descripcion: 'Esta es la primera videoconferencia planificada', hora_inicio: '09:00 AM', hora_fin: '11:30 AM', fecha: '2022-09-30', mannana: 0, tarde: 1 },
-    { id: 5, nombre: 'Primera', descripcion: 'Esta es la primera videoconferencia planificada', hora_inicio: '09:00 AM', hora_fin: '11:30 AM', fecha: '2022-09-28', mannana: 1, tarde: 1 },
-  ];
+  @Input() selected: any;
+  @Input() not_allowed: any[] = [];
+  // dates: any[] = [
+  //   { id: 5, nombre: 'Primera', descripcion: 'Esta es la primera videoconferencia planificada', hora_inicio: '09:00 AM', hora_fin: '11:30 AM', fecha: '2022-09-29', mannana: 1, tarde: 0 },
+  //   { id: 5, nombre: 'Primera', descripcion: 'Esta es la primera videoconferencia planificada', hora_inicio: '09:00 AM', hora_fin: '11:30 AM', fecha: '2022-09-30', mannana: 0, tarde: 1 },
+  //   { id: 5, nombre: 'Primera', descripcion: 'Esta es la primera videoconferencia planificada', hora_inicio: '09:00 AM', hora_fin: '11:30 AM', fecha: '2022-09-28', mannana: 1, tarde: 1 },
+  // ];
+  @Input() dates: any[]=[];
+  @Output() emisor: EventEmitter<any> = new EventEmitter();
 
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.rellenarCalendario();
-    this.getDates();
   }
 
   rellenarCalendario() {
@@ -55,13 +56,7 @@ export class CalendarioComponent implements OnInit {
     this.rellenarCalendario();
   }
 
-  getDates() {
-    // this.api.getVideoConferencias().subscribe(result => {
-    //   this.dates = result;
-    //   console.log(result);
 
-    // })
-  }
 
   cambiarAnno(opcion: string) {
     if (opcion == 'mas') this.anno++; else this.anno--;
@@ -69,7 +64,13 @@ export class CalendarioComponent implements OnInit {
 
   select(dia: number) {
     this.day = dia;
+    const date ={
+      day: dia,
+      month: this.month,
+      anno: this.anno
+    }
     this.selected = moment(moment(this.anno + '-' + ((this.month + 1) < 10 ? '0' + (this.month + 1) : (this.month + 1)) + '-' + ((this.day < 10) ? '0' + this.day : this.day)))
+    this.emisor.emit(date);
   }
 
   not_alloweds(day: number) {
@@ -86,11 +87,11 @@ export class CalendarioComponent implements OnInit {
     day.detalles = [];
     let entero = false;
     this.dates.forEach(item => {
-      if (moment(item.fecha).date() === day.dia && moment(item.fecha).month() === this.month && (item.mannana && item.tarde)) {
+      if (moment(item.fecha).date() === day.dia && moment(item.fecha).month() === this.month && moment(item.fecha).year() == this.anno && (item.mannana && item.tarde)) {
         entero = true;
-        day.detalles.push(`videoconferencia:${item.nombre} \n descripcion: ${item.descripcion} \n horario: ${item.hora_inicio} - ${item.hora_fin}`)
-      } else if (moment(item.fecha).date() === day.dia && moment(item.fecha).month() === this.month && (item.mannana || item.tarde)) {
-        day.detalles.push(`videoconferencia:${item.nombre} \n descripcion: ${item.descripcion} \n horario: ${item.hora_inicio} - ${item.hora_fin}`)
+        day.detalles.push(`videoconferencia:${item.nombre} \n descripcion: ${item.descripcion} \n horario: ${moment(item.hora_inicio).format('HH:mm a')} - ${moment(item.hora_fin).format('HH:mm a')}`)
+      } else if (moment(item.fecha).date() === day.dia && moment(item.fecha).month() === this.month && moment(item.fecha).year() == this.anno && (item.mannana || item.tarde)) {
+        day.detalles.push(`videoconferencia:${item.nombre} \n descripcion: ${item.descripcion} \n horario: ${moment(item.hora_inicio).format('HH:mm a')} - ${moment(item.hora_fin).format('HH:mm a')}`)
         cont++;
       }
 
--- src/app/pages/menu/solicitud/solicitud.component.ts
+++ src/app/pages/menu/solicitud/solicitud.component.ts
@@ -17,19 +17,44 @@ export class SolicitudComponent implements OnInit {
     citado_por: '',
     mannana: true,
     tarde: false,
-    hora_inicio: '',
-    hora_fin: '',
+    hora_inicio: 8,
+    minuto_inicio: 0,
+    hora_fin: 8,
+    minuto_fin: 0,
     estado: 'en espera',
-    not_allowed: 0
+    not_allowed: 0,
+    cant_personas: '',
   };
+  horario: number = 0;
+  now = moment();
 
-  constructor(private api: ApiService,private storage: SessionStorageService, private router: Router) { }
+  disable_all: boolean = false;
+  disable: boolean = false;
+
+  not_allowed: any[] = [moment('2022-10-23')];
+  selected: any = this.now;
+  dates: any[] = [];
+
+  horas_inicio: number[] = []
+  minutos_inicio: number[] = [];
+  horas_fin: number[] = []
+  minutos_fin: number[] = [];
+
+  constructor(private api: ApiService, private storage: SessionStorageService, private router: Router) { }
 
   ngOnInit(): void {
-
+    this.getDates()
+this.rellenarHorasMin();
   }
 
-  addVideoConferencia(){
+  getDates() {
+    this.api.getVideoConferencias().subscribe(result => {
+      this.dates = result;
+      console.log(result);
+    })
+  }
+
+  addVideoConferencia() {
     console.log(this.solicitud);
     const formData = new FormData();
     formData.append('nombre', this.solicitud.nombre);
@@ -41,13 +66,54 @@ export class SolicitudComponent implements OnInit {
     formData.append('hora_fin', this.solicitud.hora_fin);
     formData.append('estado', this.solicitud.estado);
     formData.append('not_allowed', this.solicitud.not_allowed);
-    this.api.addVideoConferencia(formData).subscribe(result=>{
+    this.api.addVideoConferencia(formData).subscribe(result => {
       console.log(result);
-      
-    }, error=>{
+
+    }, error => {
       console.log(error);
-      
     })
   }
 
+  select(date: any) {
+    this.selected = moment(moment(date.anno + '-' + ((date.month + 1) < 10 ? '0' + (date.month + 1) : (date.month + 1)) + '-' + ((date.day < 10) ? '0' + date.day : date.day)))
+
+    const l = this.dates.filter(e => {
+      return moment(e.fecha).format('yyyy-MM-DD') == this.selected.format('yyyy-MM-DD')
+    });
+
+    // console.log(l[0]);
+    if (l.length > 0) {
+      this.solicitud.mannana = l[0].mannana
+      this.solicitud.tarde = l[0].tarde
+      this.horario = this.solicitud.mannana && this.solicitud.tarde ? 2 : this.solicitud.tarde ? 1 : 0
+      this.disable = true
+    } else {
+      this.solicitud.mannana = 0
+      this.solicitud.tarde = 0
+      this.horario = 0
+      this.disable = false
+    }
+  }
+
+  rellenarHorasMin() {
+    console.log(this.solicitud.hora_inicio);
+    
+    if (this.horario == 0) {
+      this.horas_inicio = Array(5).fill(8).map((x, i) => x + i);
+      this.minutos_inicio = Array(7).fill(0).map((x, i) => i*10);
+      this.horas_fin = Array(13-Number(this.solicitud.hora_inicio)).fill(Number(this.solicitud.hora_inicio)).map((x, i) => x + i);
+      this.minutos_fin = Array(7-(Number(this.solicitud.minuto_inicio)/10)).fill(Number(this.solicitud.minuto_inicio)).map((x, i) => (x/10+i)*10);
+    }
+    else if(this.horario == 1){
+      this.horas_inicio = Array(5).fill(1).map((x, i) => x + i);
+      this.minutos_inicio = Array(7).fill(0).map((x, i) => i*10);
+      this.horas_fin = Array(6-Number(this.solicitud.hora_inicio)).fill(Number(this.solicitud.hora_inicio)).map((x, i) => x + i);
+      this.minutos_fin = Array(7-(Number(this.solicitud.minuto_inicio)/10)).fill(Number(this.solicitud.minuto_inicio)).map((x, i) => (x/10+i)*10);
+    }else{
+      this.horas_inicio = Array(10).fill(8).map((x, i) => x + i);
+      this.minutos_inicio = Array(7).fill(0).map((x, i) => i*10);
+      this.horas_fin = Array(18-Number(this.solicitud.hora_inicio)).fill(Number(this.solicitud.hora_inicio)).map((x, i) => x + i);
+      this.minutos_fin = Array(7-(Number(this.solicitud.minuto_inicio)/10)).fill(Number(this.solicitud.minuto_inicio)).map((x, i) => (x/10+i)*10);
+    }
+  }
 }
